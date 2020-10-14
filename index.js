e56ab581b2d889ef32c6d1c9f9c14f5b5295c6e0
TodiricaOana <48209410+TodiricaOana@users.noreply.github.com>
2020-10-14 19:29:07 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const {message} = require('./src/message');
+
+app.get('/message', (req, res) => {
+    message()
+        .then((body) => {
+        const { text } = body;
+        res.send(text);
+    })
+});
+
+app.listen(port, () => {
+    console.log("Server is listening");
+})
